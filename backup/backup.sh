096a588a8d14bae578679eb77e041a67e9ed3420
Piotr Szulc <szulo123@gmail.com>
2021-06-15 23:54:41 +0200

    Added rclone to send to one drive

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-OUTPUT="$BACKUP_DIR./$(date +%Y%m%d%H%M%S)"
+DIR_NAME=$(date +%Y%m%d%H%M%S)
+OUTPUT=$(realpath "$BACKUP_DIR./$DIR_NAME")
+DESTINATION="one-drive:backup/smart-home/$DIR_NAME"
+RCLONE_CONFIG="$(pwd)/config"
 IMAGE="alpine:3.13"
+
+echo "Backup root: $BACKUP_DIR"
+echo "Backup directory: $OUTPUT"
+echo "Backup destination: $DESTINATION"
+echo "rclone config: $RCLONE_CONFIG"
+
+read -p "Press [Enter] key to start backup..."
 
 mkdir -p $OUTPUT
 
@@ -17,3 +27,5 @@ docker start grafana
 
 docker exec influxdb bash -c "rm -fv /backup/* && influxd backup --portable /backup/"
 docker run --rm -v smart-home_db-backup:/db-backup -v $(pwd):/backup $IMAGE sh -c "cd /db-backup && tar cvjf /backup/influxdb.tar.bz2 ."
+
+docker run --rm -v $RCLONE_CONFIG:/config/rclone -v $BACKUP_DIR:/data:ro --user $(id -u):$(id -g) rclone/rclone copy -P /data/$DIR_NAME $DESTINATION
