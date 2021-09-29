3642e24e7e4f4fd9d7122fd2b9681ce24a7c2a2a
Piotr Szulc <szulo123@gmail.com>
2021-09-29 23:30:22 +0200

    Less output in backup script

--- backup/backup.sh
+++ backup/backup.sh
@@ -11,21 +11,23 @@ echo "Backup directory: $OUTPUT"
 echo "Backup destination: $DESTINATION"
 echo "rclone config: $RCLONE_CONFIG"
 
-read -p "Press [Enter] key to start backup..."
+# read -p "Press [Enter] key to start backup..."
 
-mkdir -p $OUTPUT
+mkdir -p "$OUTPUT"
 
-cd $OUTPUT
+cd "$OUTPUT"
+
+echo `pwd`
 
 docker stop mosquitto
-docker run --rm --volumes-from mosquitto -v $(pwd):/backup $IMAGE sh -c "cd /mosquitto/data && tar cvjf /backup/mosquitto.tar.bz2 ."
+docker run --rm --volumes-from mosquitto -v $(pwd):/backup $IMAGE sh -c "cd /mosquitto/data && tar cjf /backup/mosquitto.tar.bz2 ."
 docker start mosquitto
 
 docker stop grafana
-docker run --rm --volumes-from grafana -v $(pwd):/backup $IMAGE sh -c "cd /var/lib/grafana && tar cvjf /backup/grafana.tar.bz2 ."
+docker run --rm --volumes-from grafana -v $(pwd):/backup $IMAGE sh -c "cd /var/lib/grafana && tar cjf /backup/grafana.tar.bz2 ."
 docker start grafana
 
-docker exec influxdb bash -c "rm -fv /backup/* && influxd backup --portable /backup/"
-docker run --rm -v smart-home_db-backup:/db-backup -v $(pwd):/backup $IMAGE sh -c "cd /db-backup && tar cvjf /backup/influxdb.tar.bz2 ."
+docker exec influxdb bash -c "rm -f /backup/* && influxd backup --portable /backup/"
+docker run --rm -v smart-home_db-backup:/db-backup -v $(pwd):/backup $IMAGE sh -c "cd /db-backup && tar cjf /backup/influxdb.tar.bz2 ."
 
 docker run --rm -v $RCLONE_CONFIG:/config/rclone -v $BACKUP_DIR:/data:ro --user $(id -u):$(id -g) rclone/rclone copy -P /data/$DIR_NAME $DESTINATION
