dda3d60955165ffd89294e0f16969f57200a9554
Piotr Szulc <szulo123@gmail.com>
2021-10-18 20:31:45 +0200

    Added node-red backup

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 DIR_NAME=$(date +%Y%m%d%H%M%S)
 OUTPUT=$(realpath "$BACKUP_DIR./$DIR_NAME")
 DESTINATION="one-drive:backup/smart-home/$DIR_NAME"
@@ -29,5 +27,9 @@ docker start grafana
 
 docker exec influxdb bash -c "rm -f /backup/* && influxd backup --portable /backup/"
 docker run --rm -v smart-home_db-backup:/db-backup -v $(pwd):/backup $IMAGE sh -c "cd /db-backup && tar cjf /backup/influxdb.tar.bz2 ."
+
+docker cp node-red:/data ./
+tar cjf node-red.tar.bz2 data
+rm -rf data
 
 docker run --rm -v $RCLONE_CONFIG:/config/rclone -v $BACKUP_DIR:/data:ro --user $(id -u):$(id -g) rclone/rclone copy -P /data/$DIR_NAME $DESTINATION
